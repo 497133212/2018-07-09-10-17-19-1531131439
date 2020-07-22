936a4b378a9f57a5efed1e1907cd83162199d492
Austin <497133212@qq.com>
2020-07-22 22:22:17 +0800

    [practice6][Austin] feature: Add Person,Teacher,Student,Klass and test method

--- src/main/java/practice07/Student.java
+++ src/main/java/practice07/Student.java
@@ -1,4 +1,22 @@
 package practice07;
 
-public class Student {
+public class Student extends Person{
+    protected Klass klass;
+
+    public Student(String name, int age) {
+        super(name, age);
+    }
+
+    public Student(String name, int age, Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public String introduce() {
+        return super.introduce() + String.format(" I am a Student. I am at Class %s.", this.klass.number);
+    }
 }
--- src/main/java/practice07/Teacher.java
+++ src/main/java/practice07/Teacher.java
@@ -1,4 +1,34 @@
 package practice07;
 
-public class Teacher {
+public class Teacher extends Person {
+    protected Klass klass;
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public Teacher(String name, int age, Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public String introduce() {
+        if(this.klass == null) {
+            return super.introduce() + " I am a Teacher. I teach No Class.";
+        } else {
+            return super.introduce() + String.format(" I am a Teacher. I teach Class %s.", this.klass.number);
+        }
+    }
+
+    public String introduceWith(Student student) {
+        if(this.klass.number == student.klass.number) {
+            return String.format("My name is %s. I am %s years old. I am a Teacher. I teach %s.", this.name, this.age, student.name);
+        } else{
+            return String.format("My name is %s. I am %s years old. I am a Teacher. I don't teach %s.", this.name, this.age, student.name);
+        }
+    }
 }
