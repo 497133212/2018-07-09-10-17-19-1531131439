d2feb9db003d52c3690574e955086148282d867a
Austin <497133212@qq.com>
2020-07-22 16:02:09 +0800

    [practice2][Austin] feature: Add name and age in Person, klass in Student and introduce method.

--- src/main/java/practice02/Student.java
+++ src/main/java/practice02/Student.java
@@ -1,4 +1,24 @@
 package practice02;
 
-public class Student {
+public class Student extends Person{
+
+    protected int klass;
+
+    public Student(String name, int age) {
+        super(name, age);
+    }
+
+    public Student(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public String introduce() {
+        String str = "I am a Student. I am at Class " + klass + ".";
+        return str;
+    }
 }
