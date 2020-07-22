58057a8f1b33868b1bfb3a16c3b7fc750c88e41c
Austin <497133212@qq.com>
2020-07-22 22:24:12 +0800

    [practice9][Austin] feature: Add Person,Teacher,Student,Klass and test method

--- src/main/java/practice09/Klass.java
+++ src/main/java/practice09/Klass.java
@@ -1,4 +1,40 @@
 package practice09;
 
 public class Klass {
+    private int number;
+    private Student leader;
+    private String displayName;
+
+    public Klass(int number) {
+        this.number = number;
+        this.displayName = "Class 2";
+    }
+
+    public void assignLeader(Student student) {
+        if(student.getKlass().number != this.number){
+            System.out.print("It is not one of us.\n");
+        }else {
+            this.leader = student;
+        }
+    }
+
+    public void appendMember(Student student) {
+        student.getKlass().setNumber(this.number);
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
 }
