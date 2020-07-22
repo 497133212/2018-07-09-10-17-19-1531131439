4f2d9a968c4146775cd9badb4720605f07f59f33
Austin <497133212@qq.com>
2020-07-22 16:16:37 +0800

    [practice6][Austin] feature: Add name and age in Person, klass in Student and Teacher and introduce method.

--- src/main/java/practice06/Student.java
+++ src/main/java/practice06/Student.java
@@ -1,4 +1,25 @@
 package practice06;
 
-public class Student {
+import practice05.Person;
+
+public class Student extends Person {
+    protected int klass;
+
+    public Student(String name, int age) {
+        super(name, age);
+    }
+
+    public Student(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public String introduce() {
+        Person person = new Person("Tom", 21);
+        return person.introduce() + " I am a Student. I am at Class " + this.klass + ".";
+    }
 }
--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,28 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person {
+
+    protected Integer klass;
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public Teacher(String name, int age, Integer klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public String introduce() {
+        Person person = new Person("Tom", 21);
+        if(this.klass == null) {
+            return person.introduce() + " I am a Teacher. I teach No Class.";
+        } else {
+            return person.introduce() + " I am a Teacher. I teach Class " + this.klass + ".";
+        }
+    }
 }
