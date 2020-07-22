1d9599e1753669fdf8f9a5e28b406ddc38ab4cab
Austin <497133212@qq.com>
2020-07-22 22:23:15 +0800

    [practice8][Austin] feature: Add Person,Teacher,Student,Klass and test method

--- src/main/java/practice08/Person.java
+++ src/main/java/practice08/Person.java
@@ -1,4 +1,46 @@
 package practice08;
 
 public class Person {
+    protected int id;
+    protected String name;
+    protected int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Person(int id, String name, int age) {
+        this.name = name;
+        this.age = age;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public boolean equals(Object obj) {
+        Person person = null;
+        if (obj instanceof Person) {
+            person = (Person) obj;
+            if (person.name == this.name && person.age == this.age && person.id == this.id)
+                return true;
+            else
+                return false;
+        }else
+            return false;
+    }
+
+    public String introduce() {
+        return String.format("My name is %s. I am %s years old.",this.name,this.age);
+    }
 }
--- src/main/java/practice08/Student.java
+++ src/main/java/practice08/Student.java
@@ -1,4 +1,26 @@
 package practice08;
 
-public class Student {
+public class Student extends Person {
+    protected Klass klass;
+
+    public Student(String name, int age) {
+        super(name, age);
+    }
+
+    public Student(int id, String name, int age, Klass klass) {
+        super(name, age);
+        this.id = id;
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public String introduce() {
+        if(klass.getLeader() == this) {
+            return super.introduce() + String.format(" I am a Student. I am Leader of Class %s.", this.klass.getNumber());
+        }
+        return super.introduce() + String.format(" I am a Student. I am at Class %s.", this.getKlass().getNumber());
+    }
 }
