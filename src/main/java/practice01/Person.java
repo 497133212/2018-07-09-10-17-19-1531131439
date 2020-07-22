d5134945b69c83ea52e2c10d77b69841f472b017
Austin <497133212@qq.com>
2020-07-22 22:11:30 +0800

    [practice1][Austin] feature: Add name and age in Person,

--- src/main/java/practice01/Person.java
+++ src/main/java/practice01/Person.java
@@ -18,7 +18,7 @@ public class Person {
     }
 
     public String introduce() {
-        return "My name is " + name + ". I am " + age + " years old.";
+        return String.format("My name is %s. I am %s years old.",this.name,this.age);
 
     }
 }
