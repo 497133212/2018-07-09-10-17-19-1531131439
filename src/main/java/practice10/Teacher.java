f8be88e7bfa026c3530b842d3910bcbd357b7af8
Austin <497133212@qq.com>
2020-07-22 22:24:52 +0800

    [practice10][Austin] feature: Add Person,Teacher,Student,Klass and test method

--- src/main/java/practice10/Teacher.java
+++ src/main/java/practice10/Teacher.java
@@ -1,4 +1,58 @@
 package practice10;
 
-public class Teacher {
+import java.util.LinkedList;
+
+public class Teacher extends Person{
+    private LinkedList<Klass> classes;
+
+    public Teacher(int id, String name, int age, LinkedList<Klass> classes) {
+        super(id, name, age);
+        this.classes = classes;
+    }
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    @Override
+    public String introduce() {
+
+        if (this.classes != null && this.classes.size() != 0 ) {
+            StringBuilder klasses = new StringBuilder();
+            int i = 0;
+            for (Klass klass: this.classes) {
+                i++;
+                klasses.append(klass.getNumber());
+                if(i!=this.classes.size()){
+                    klasses.append(", ");
+                }
+            }
+            return super.introduce() + String.format(" I am a Teacher. I teach Class %s.", klasses);
+        } else {
+            return super.introduce() + " I am a Teacher. I teach No Class.";
+        }
+    }
+
+    public String introduceWith(Student student) {
+        for (Klass klass: classes) {
+            if(student.getKlass().getNumber() == klass.getNumber()) {
+                return super.introduce() + String.format(" I am a Teacher. I teach %s.", student.getName());
+            }
+        }
+        return super.introduce() + String.format(" I am a Teacher. I don't teach %s.", student.getName());
+    }
+
+    public boolean isTeaching(Student student) {
+        for (Klass klass: classes) {
+            boolean isInKlass = klass.isIn(student);
+            if(isInKlass){
+                return isInKlass;
+            }
+        }
+        return false;
+    }
+
+    public LinkedList<Klass> getClasses() {
+        return classes;
+    }
 }
